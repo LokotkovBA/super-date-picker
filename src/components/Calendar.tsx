6efa04205f3e78cb056e9f3da84994624d96f09d
Boris Lokotkov <woweuro2010@gmail.com>
2023-04-20 00:45:43 +0300

    add clsx

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,26 +1,38 @@
 import React, { useState } from "react";
 import { getCalendarDates } from "~/utils/dateFunctions";
+import clsx from "clsx";
+
+const daysOfWeek = ["MO", "TU", "WE", "TH", "FR", "SA", "SU"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const [datesArray, monthsArray, todayIndex] = getCalendarDates(Date.now());
+
+function dateStyles(isToday: boolean, isUserSelected: boolean, isSelectedMonth: boolean) {
+    return clsx("cursor-pointer border-2 border-slate-950 duration-200 hover:scale-125 hover:font-bold hover:underline p-1.5 leading-none w-8 text-center",
+        {
+            "text-sky-500 font-bold": (!isUserSelected && isToday),
+            "bg-sky-700 border-sky-500 rounded": isUserSelected,
+            "text-slate-50": isSelectedMonth,
+            "text-slate-500": !isSelectedMonth
+        });
+}
+
 type CalendarProps = {
     selectedDate: number
 }
-const daysOfWeek = ["MO", "TU", "WE", "TH", "FR", "SA", "SU"];
-const [datesArray, monthsArray, todayIndex] = getCalendarDates(Date.now());
-
 const Calendar: React.FC<CalendarProps> = () => {
-    const [userSelectedDate, setUserSelectedDate] = useState(todayIndex);
+    const [userSelectedDateIndex, setUserSelectedDateIndex] = useState(todayIndex);
     return (
         <div className="max-w-xs bg-slate-950 grid grid-cols-7 gap-1 justify-items-center p-3">
             {daysOfWeek.map(day => {
-                return <div className="text-slate-500">{day}</div>;
+                return <div key={day} className="text-slate-500">{day}</div>;
             })}
             {datesArray.map((date, index) => {
-                return <div onClick={() => setUserSelectedDate(index)} className={`cursor-pointer border-2 border-slate-950 duration-200 hover:scale-125 hover:font-bold
-                ${index === userSelectedDate ? "bg-sky-700 border-sky-500 rounded" : ""}
-                ${index !== userSelectedDate && index === todayIndex ? "text-sky-500 font-bold" : ""}
-                ${monthsArray[index] === 0 ? "text-slate-50" : " text-slate-500"}
-                hover:underline p-1.5 leading-none`} key={index}>{date}</div>;
+                return <div onClick={() => setUserSelectedDateIndex(index)}
+                    className={dateStyles(index === todayIndex, index === userSelectedDateIndex, monthsArray[index] === 0)} key={index}>
+                    {date}
+                </div>;
             })}
-        </div>
+        </div >
     );
 };
 
