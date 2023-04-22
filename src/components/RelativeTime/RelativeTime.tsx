dbaa0b800db0a16c15aeab6fc5364132dd96e872
Boris Lokotkov <woweuro2010@gmail.com>
2023-04-22 19:11:54 +0300

    reorganize RelativeTime

--- src/components/RelativeTime/RelativeTime.tsx
+++ src/components/RelativeTime/RelativeTime.tsx
@@ -1,9 +1,15 @@
 import React, { useLayoutEffect, useState } from "react";
-import { type ModeProps } from "./SuperDatePicker";
 import Cross from "~/assets/Cross";
 import CheckMark from "~/assets/CheckMark";
 import clsx from "clsx";
+import { type ModeProps } from "../SuperDatePicker";
+import { options, setTimeRelative, units } from "./utils";
 
+function onChangeRounded(newRounded: boolean, selectedOption: number, diff: number, dateSetter: (date: Date) => void) {
+    const unitIndex = selectedOption % 7;
+    const relativeDiff = selectedOption === unitIndex ? -diff : diff;
+    setTimeRelative(units[unitIndex], relativeDiff, newRounded, dateSetter);
+}
 
 const inputStyles = "w-44 h-9 p-1 bg-neutral-800 focus:outline-none border-b-2 border-neutral-800 focus:border-sky-950";
 const RelativeTime: React.FC<ModeProps> = ({ dateSetter }) => {
@@ -22,7 +28,7 @@ const RelativeTime: React.FC<ModeProps> = ({ dateSetter }) => {
         const unitIndex = selectedOption % 7;
         setDiffValue(newDiff);
         const relativeDiff = selectedOption === unitIndex ? -newDiff : newDiff;
-        setRelative(units[unitIndex], relativeDiff, rounded, dateSetter);
+        setTimeRelative(units[unitIndex], relativeDiff, rounded, dateSetter);
     }
 
     function onSelectChange(event: React.ChangeEvent<HTMLSelectElement>) {
@@ -30,7 +36,7 @@ const RelativeTime: React.FC<ModeProps> = ({ dateSetter }) => {
         setSelectedOption(newOption);
         const unitIndex = newOption % 7;
         const relativeDiff = newOption === unitIndex ? -diffValue : diffValue;
-        setRelative(units[unitIndex], relativeDiff, rounded, dateSetter);
+        setTimeRelative(units[unitIndex], relativeDiff, rounded, dateSetter);
     }
 
     function onRoundedClick() {
@@ -39,7 +45,7 @@ const RelativeTime: React.FC<ModeProps> = ({ dateSetter }) => {
     }
 
     useLayoutEffect(() => {
-        setRelative("minute", -30, false, dateSetter);
+        setTimeRelative("minute", -30, false, dateSetter);
     }, [dateSetter]);
 
     return (
@@ -72,79 +78,4 @@ const RelativeTime: React.FC<ModeProps> = ({ dateSetter }) => {
 };
 export default RelativeTime;
 
-function onChangeRounded(newRounded: boolean, selectedOption: number, diff: number, dateSetter: (date: Date) => void) {
-    const unitIndex = selectedOption % 7;
-    const relativeDiff = selectedOption === unitIndex ? -diff : diff;
-    setRelative(units[unitIndex], relativeDiff, newRounded, dateSetter);
-}
 
-function setRelative(unit: typeof units[number], value: number, rounded: boolean, dateSetter: (date: Date) => void) {
-    const date = new Date();
-    let diff = value;
-    switch (unit) {
-        case "year":
-            date.setFullYear(date.getFullYear() + diff);
-            diff = rounded ? -date.getMonth() : 0;
-        // falls through
-        case "month":
-            date.setMonth(date.getMonth() + diff);
-            diff = rounded ? -date.getDate() + 1 : 0;
-        // falls through
-        case "day":
-            date.setDate(date.getDate() + diff);
-            diff = 0;
-        // falls through
-        case "week":
-            if (diff !== 0) {
-                let currentWeekDay = date.getDay() - 1;
-                if (currentWeekDay < 0) {
-                    currentWeekDay = 6;
-                }
-                const weekDiff = diff * 7 - (rounded ? currentWeekDay : 0);
-                date.setDate(date.getDate() + weekDiff);
-            }
-            diff = rounded ? -date.getHours() : 0;
-        // falls through
-        case "hour":
-            date.setHours(date.getHours() + diff);
-            diff = rounded ? -date.getMinutes() : 0;
-        // falls through
-        case "minute":
-            date.setMinutes(date.getMinutes() + diff);
-            diff = rounded ? -date.getSeconds() : 0;
-        // falls through
-        case "second":
-            date.setSeconds(date.getSeconds() + diff);
-    }
-    if (rounded) {
-        date.setMilliseconds(0);
-    }
-    dateSetter(date);
-}
-
-const options = [
-    "Seconds ago",
-    "Minutes ago",
-    "Hours ago",
-    "Days ago",
-    "Weeks ago",
-    "Months ago",
-    "Years ago",
-    "Seconds from now",
-    "Minutes from now",
-    "Hours from now",
-    "Days from now",
-    "Weeks from now",
-    "Months from now",
-    "Years from now",
-] as const;
-
-const units = [
-    "second",
-    "minute",
-    "hour",
-    "day",
-    "week",
-    "month",
-    "year",
-] as const;
---
+++ src/components/RelativeTime/utils.ts
@@ -0,0 +1,71 @@
+export const options = [
+    "Seconds ago",
+    "Minutes ago",
+    "Hours ago",
+    "Days ago",
+    "Weeks ago",
+    "Months ago",
+    "Years ago",
+    "Seconds from now",
+    "Minutes from now",
+    "Hours from now",
+    "Days from now",
+    "Weeks from now",
+    "Months from now",
+    "Years from now",
+] as const;
+
+export const units = [
+    "second",
+    "minute",
+    "hour",
+    "day",
+    "week",
+    "month",
+    "year",
+] as const;
+
+export function setTimeRelative(unit: typeof units[number], value: number, rounded: boolean, dateSetter: (date: Date) => void) {
+    const date = new Date();
+    let diff = value;
+    switch (unit) {
+        case "year":
+            date.setFullYear(date.getFullYear() + diff);
+            diff = rounded ? -date.getMonth() : 0;
+        // falls through
+        case "month":
+            date.setMonth(date.getMonth() + diff);
+            diff = rounded ? -date.getDate() + 1 : 0;
+        // falls through
+        case "day":
+            date.setDate(date.getDate() + diff);
+            diff = 0;
+        // falls through
+        case "week":
+            if (diff !== 0) {
+                let currentWeekDay = date.getDay() - 1;
+                if (currentWeekDay < 0) {
+                    currentWeekDay = 6;
+                }
+                const weekDiff = diff * 7 - (rounded ? currentWeekDay : 0);
+                date.setDate(date.getDate() + weekDiff);
+            }
+            diff = rounded ? -date.getHours() : 0;
+        // falls through
+        case "hour":
+            date.setHours(date.getHours() + diff);
+            diff = rounded ? -date.getMinutes() : 0;
+        // falls through
+        case "minute":
+            date.setMinutes(date.getMinutes() + diff);
+            diff = rounded ? -date.getSeconds() : 0;
+        // falls through
+        case "second":
+            date.setSeconds(date.getSeconds() + diff);
+    }
+    if (rounded) {
+        date.setMilliseconds(0);
+    }
+    dateSetter(date);
+}
+
