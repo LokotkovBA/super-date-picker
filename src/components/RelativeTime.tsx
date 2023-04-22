c37469b19e5e49879162c9019e01b24c75ac538e
Boris Lokotkov <woweuro2010@gmail.com>
2023-04-22 19:03:15 +0300

    implement relative time picker

--- src/components/RelativeTime.tsx
+++ src/components/RelativeTime.tsx
@@ -1,30 +1,63 @@
-import React, { useState } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { type ModeProps } from "./SuperDatePicker";
 import Cross from "~/assets/Cross";
 import CheckMark from "~/assets/CheckMark";
 import clsx from "clsx";
 
 
-const inputStyles = "w-44 bg-neutral-800 p-1";
-const RelativeTime: React.FC<ModeProps> = ({ selectedDate, dateSetter }) => {
+const inputStyles = "w-44 h-9 p-1 bg-neutral-800 focus:outline-none border-b-2 border-neutral-800 focus:border-sky-950";
+const RelativeTime: React.FC<ModeProps> = ({ dateSetter }) => {
     const [rounded, setRounded] = useState(false);
     const [selectedOption, setSelectedOption] = useState(1);
+    const [diffValue, setDiffValue] = useState(30);
+    const [showWarning, setShowWarning] = useState(false);
+
+    function onInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const newDiff = parseInt(event.target.value);
+        if (Number.isNaN(newDiff)) {
+            setShowWarning(true);
+            return;
+        }
+        setShowWarning(false);
+        const unitIndex = selectedOption % 7;
+        setDiffValue(newDiff);
+        const relativeDiff = selectedOption === unitIndex ? -newDiff : newDiff;
+        setRelative(units[unitIndex], relativeDiff, rounded, dateSetter);
+    }
+
+    function onSelectChange(event: React.ChangeEvent<HTMLSelectElement>) {
+        const newOption = parseInt(event.target.value);
+        setSelectedOption(newOption);
+        const unitIndex = newOption % 7;
+        const relativeDiff = newOption === unitIndex ? -diffValue : diffValue;
+        setRelative(units[unitIndex], relativeDiff, rounded, dateSetter);
+    }
+
+    function onRoundedClick() {
+        setRounded(prevRounded => !prevRounded);
+        onChangeRounded(!rounded, selectedOption, diffValue, dateSetter);
+    }
+
+    useLayoutEffect(() => {
+        setRelative("minute", -30, false, dateSetter);
+    }, [dateSetter]);
 
     return (
         <section className="p-2 flex flex-col gap-5 items-start">
-            <h2 className="flex justify-around self-stretch">
-                <input defaultValue={30} className={inputStyles} type="number" />
-                <select onChange={(event) => setSelectedOption(parseInt(event.target.value))} defaultValue="1" className={inputStyles}>
+            <h2 className="grid grid-cols-2 self-stretch">
+                <input onChange={onInputChange} defaultValue={diffValue} className={inputStyles} type="number" min={0} />
+                <select onChange={onSelectChange} defaultValue="1" className={`place-self-end ${inputStyles}`}>
                     {options.map((option, index) => {
                         return <option value={index} key={option}>{option}</option>;
                     })}
                 </select>
+                {showWarning && <p className="text-sm text-red-500">Must be a number</p>}
             </h2>
             <div className="flex items-center gap-2">
                 <button className={clsx("py-1 px-4 rounded-xl", {
                     "bg-sky-900": rounded,
                     "bg-neutral-800": !rounded
-                })} onClick={() => setRounded(prev => !prev)}>
+                })} onClick={onRoundedClick}>
                     <div className={clsx("transition-all ease-in-out", {
                         "translate-x-2": rounded,
                         "-translate-x-2": !rounded
@@ -32,12 +65,62 @@ const RelativeTime: React.FC<ModeProps> = ({ selectedDate, dateSetter }) => {
                         {rounded ? <CheckMark size="1rem" className="fill-sky-500" /> : <Cross className="fill-neutral-400" size="1rem" />}
                     </div>
                 </button>
-                <span> Round to the {roundText[selectedOption % 7]}</span>
+                <span> Round to the {units[selectedOption % 7]}</span>
             </div>
         </section>
     );
 };
 export default RelativeTime;
+
+function onChangeRounded(newRounded: boolean, selectedOption: number, diff: number, dateSetter: (date: Date) => void) {
+    const unitIndex = selectedOption % 7;
+    const relativeDiff = selectedOption === unitIndex ? -diff : diff;
+    setRelative(units[unitIndex], relativeDiff, newRounded, dateSetter);
+}
+
+function setRelative(unit: typeof units[number], value: number, rounded: boolean, dateSetter: (date: Date) => void) {
+    const date = new Date();
+    let diff = value;
+    switch (unit) {
+        case "year":
+            date.setFullYear(date.getFullYear() + diff);
+            diff = rounded ? -date.getMonth() : 0;
+        // falls through
+        case "month":
+            date.setMonth(date.getMonth() + diff);
+            diff = rounded ? -date.getDate() + 1 : 0;
+        // falls through
+        case "day":
+            date.setDate(date.getDate() + diff);
+            diff = 0;
+        // falls through
+        case "week":
+            if (diff !== 0) {
+                let currentWeekDay = date.getDay() - 1;
+                if (currentWeekDay < 0) {
+                    currentWeekDay = 6;
+                }
+                const weekDiff = diff * 7 - (rounded ? currentWeekDay : 0);
+                date.setDate(date.getDate() + weekDiff);
+            }
+            diff = rounded ? -date.getHours() : 0;
+        // falls through
+        case "hour":
+            date.setHours(date.getHours() + diff);
+            diff = rounded ? -date.getMinutes() : 0;
+        // falls through
+        case "minute":
+            date.setMinutes(date.getMinutes() + diff);
+            diff = rounded ? -date.getSeconds() : 0;
+        // falls through
+        case "second":
+            date.setSeconds(date.getSeconds() + diff);
+    }
+    if (rounded) {
+        date.setMilliseconds(0);
+    }
+    dateSetter(date);
+}
 
 const options = [
     "Seconds ago",
@@ -54,9 +137,9 @@ const options = [
     "Weeks from now",
     "Months from now",
     "Years from now",
-];
+] as const;
 
-const roundText = [
+const units = [
     "second",
     "minute",
     "hour",
@@ -64,4 +147,4 @@ const roundText = [
     "week",
     "month",
     "year",
-];
+] as const;
--- src/components/SuperDatePicker.tsx
+++ src/components/SuperDatePicker.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Calendar from "./Calendar";
 import clsx from "clsx";
 import RelativeTime from "./RelativeTime";
@@ -22,18 +22,24 @@ type SuperDatePickerProps = {
 
 function parseDate(date: Date) {
     const datePart = date.toDateString().slice(4);
-    const timePart = date.toTimeString().slice(0, 8);
-    return `${datePart} @ ${timePart}`;
+    const hours = date.getHours();
+    let hoursPart = hours.toString();
+    if (hours < 10) {
+        hoursPart = `0${hoursPart}`;
+    }
+    const mmssmsPart = date.toISOString().slice(13, -1);
+    return `${datePart} @ ${hoursPart}${mmssmsPart}`;
 }
 
 const SuperDatePicker: React.FC<SuperDatePickerProps> = ({ date, setSelectedDate }) => {
     const [selectedMode, setSelectedMode] = useState(1);
     const [showedDate, setShowedDate] = useState(date);
 
-    function dateSetter(newDate: Date) {
+    const dateSetter = useCallback((newDate: Date) => {
         setShowedDate(newDate);
         setSelectedDate(newDate);
-    }
+    }, [setSelectedDate]);
+
     return (
         <article className="font-sans w-96 bg-neutral-900 text-neutral-50 rounded flex flex-col">
             <h2 className="grid grid-cols-3">
@@ -44,7 +50,7 @@ const SuperDatePicker: React.FC<SuperDatePickerProps> = ({ date, setSelectedDate
             {selectedMode === 0 && <Calendar selectedDate={date} dateSetter={dateSetter} />}
             {selectedMode === 1 && <RelativeTime selectedDate={date} dateSetter={dateSetter} />}
             <div className="flex p-2 text-sm items-center">
-                <label className="bg-sky-950 border border-sky-950 px-1 py-1 text-neutral-200 font-semibold " htmlFor="selectedDate">Selected date</label>
+                <label className="bg-sky-950 border border-sky-950 px-1 py-1 text-neutral-200 font-semibold" htmlFor="selectedDate">Selected date</label>
                 <input className="grow bg-transparent border border-sky-950 px-2 py-1" name="selectedDate" disabled={true} value={parseDate(showedDate)} />
             </div>
         </article>
